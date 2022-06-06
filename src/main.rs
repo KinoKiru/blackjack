9fc7d2ba090d0714690493f1ae5613db00554b0f
KinoKiru <youriboulanger@gmail.com>
2022-06-07 01:09:45 +0200

    kinda finished base game

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,65 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    let mut dealer: u8 = 0;
-    let mut user: u8 = 0;
-    let range = 1..11;
     loop {
-        println!("dealer {}", dealer);
-        match dealer.cmp(&17) {
-            Ordering::Less => {
-                dealer += rand::thread_rng().gen_range(range.clone());
-            }
-            Ordering::Greater => break,
-            Ordering::Equal => break,
-        }
-    }
+        let range = 1..11;
+        let mut dealer: u8 = rand::thread_rng().gen_range(range.clone());
+        let mut user: u8 = 0;
+        loop {
+            println!("dealer {}", dealer);
 
-    //initial draw for the user
-    user += rand::thread_rng().gen_range(range.clone());
-    loop {
-        //ask the user for a action based on there drawn value
-        println!("you drew {} what will you do?", user);
-
-        println!("stand or hit");
-        let mut action = String::new();
-        io::stdin()
-            .read_line(&mut action)
-            .expect("failed to read line");
-        action = action.trim().to_string();
-        if action == "hit" {
+            //initial draw for the user
             user += rand::thread_rng().gen_range(range.clone());
             if user > 21 {
                 println!("bust! \r\nDealer won!");
                 break;
             }
-            continue;
-        } else {
-            if user > dealer {
-                println!("You've won!");
-                break;
-            } else if user == dealer {
-                println!("Draw!");
-                break;
+            //ask the user for a action based on there drawn value
+            println!("you drew {} what will you do?", user);
+
+            println!("stand or hit");
+            let mut action = String::new();
+            io::stdin()
+                .read_line(&mut action)
+                .expect("failed to read line");
+            action = action.trim().to_string();
+            if action == "hit" {
+                continue;
             } else {
-                println!("You've lost!");
-                break;
+                loop {
+                    match dealer.cmp(&17) {
+                        Ordering::Less => {
+                            dealer += rand::thread_rng().gen_range(range.clone());
+                            println!("Dealer drew a card,\r\ndealer's score is now: {}", dealer);
+                            continue;
+                        }
+                        Ordering::Greater => break,
+                        Ordering::Equal => break,
+                    }
+                }
+                if user > dealer {
+                    println!("You've won!");
+                    break;
+                } else if user == dealer {
+                    println!("Draw!");
+                    break;
+                } else {
+                    println!("You've lost!");
+                    break;
+                }
             }
+        }
+
+        let mut replay = String::new();
+        println!("Play again? Y[es] N[o]");
+        io::stdin()
+            .read_line(&mut replay)
+            .expect("failed to read line");
+
+        replay = replay.trim().to_lowercase().to_string();
+
+        if replay.starts_with("n") {
+            break;
         }
     }
 }
